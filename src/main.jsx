01bb3d5d7eeb637eb18a40630a849fb1c7a2893b
ersurajkumar1999 <er.surajkumar1999@gmai.com>
2024-04-22 23:37:03 +0530

    some pacages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ToastContainer />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
