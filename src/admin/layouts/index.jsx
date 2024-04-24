7bd822924a357bc1821f572f6a2dfafac9b2db3f
ersurajkumar1999 <er.surajkumar1999@gmai.com>
2024-04-25 00:47:25 +0530

    some added

--- src/admin/layouts/index.jsx
+++ src/admin/layouts/index.jsx
@@ -1,8 +1,42 @@
-
-import { Outlet } from 'react-router-dom'
 import Header from "../layouts/Header";
 import Sidebar from './Sidebar';
+import { Outlet, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getProfile } from '../../services/CommonServices';
+import { setLoading, setProfileData } from '../../store/slices/profileSlice';
+import { logout } from '../../store/slices/authSlice';
+import { setClearMessage } from '../../store/slices/alertMessageSlice';
+
 const AdminMainLayout = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+    const { profileData } = useSelector((state) => state.profile);
+    useEffect(() => {
+        if (!isAuthenticated) {
+            navigate('/auth/login');
+        }
+    })
+
+
+    useEffect(() => {
+        if (!profileData) {
+            const getProfileData = async () => {
+                dispatch(setLoading(true));
+                try {
+                    const response = await getProfile();
+                    if (response.status) {
+                        dispatch(setProfileData(response?.data?.data ?? null))
+                    }
+                } catch (error) {
+                    console.log("Error getting profile", error);
+                }
+                dispatch(setLoading(false));
+            }
+            getProfileData();
+        }
+    }, [profileData])
     return (
         <>
             <Header />
